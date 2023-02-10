9c4de95bff7edb1dd13ab936a6d60cfc5e53511d
WojtekK <Wojtek-K@users.noreply.github.com>
2023-02-10 19:45:04 +0100

    Create kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,6 @@
+# Zadanie (kalkulator 4 działaniowy)
+# W zadaniu należy napisać kalkulator wczytujący od użytkownika kolejno
+# liczbę (pierwszy operand)
+# znak +,-,* lub /
+# liczbę (drugi operand)
+# i zwracający wynik odpowiedniego działania kalkulatorowego
