896c655692fab367f19fec2f5a20986e82d88a45
ggoz57 <grzegorz.gozdziewicz@logora.edu.pl>
2023-02-11 12:03:19 +0100

    gggg

--- kalkulator.py
+++ kalkulator.py
@@ -8,9 +8,10 @@
 # Podaj nazwę 1 zmiennej: x
 # Podaj nazwę 2 zmiennej: y
 # Podaj symbol działania: znak
-# Wynik w zmiennej: wynik
 
-if znak == "+":
-    wynik = x + y
-elif znak == "-":
-    wynik = x - y
+#if znak= '*':
+ #   wynik=x*y
+#if znak="/":
+   # wynik=x/y
+
+
