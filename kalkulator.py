3a8e855f1a042b589ca7e32f4e91f623cf97f6aa
bozwiecz <Bozena.Wieczorek@polsl.pl>
2023-02-11 11:51:39 +0100

    dod i ode

--- kalkulator.py
+++ kalkulator.py
@@ -8,4 +8,9 @@
 # Podaj nazwę 1 zmiennej: x
 # Podaj nazwę 2 zmiennej: y
 # Podaj symbol działania: znak
+# Wynik w zmiennej: wynik
 
+if znak == "+":
+    wynik = x + y
+elif znak == "-":
+    wynik = x - y
